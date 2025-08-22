b830a4bffc5a30329aa0497a0f28f6637eb44871
hafizecim <hfz.snyl@gmail.com>
2025-08-22 12:04:21 +0300

    islemler/baglan.php

---
+++ islemler/baglan.php
@@ -0,0 +1,17 @@
+<?php 
+$host="localhost"; //Host adınızı girin varsayılan olarak Localhosttur eğer bilginiz yoksa bu şekilde bırakın
+$veritabani_ismi="kurs"; //Veritabanı İsminiz
+$kullanici_adi="root"; //Veritabanı kullanıcı adınız
+$sifre = ""; //Kullanıcı şifreniz şifre yoksa 123456789 yazan yeri silip boş bırakın
+
+try {
+	$db=new PDO("mysql:host=$host;dbname=$veritabani_ismi;charset=utf8",$kullanici_adi,$sifre);
+	echo "veritabanı bağlantısı başarılı";
+}
+
+catch (PDOExpception $e) {
+    echo "veritabanı bağlantısı başarısız";
+	echo $e->getMessage();
+}
+
+?>
